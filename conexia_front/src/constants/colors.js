fd5b1c926cd39d21072c9f763ffa59729dc8bd91
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-06-16 17:07:09 -0300

    Añadiendo los colores del proyecto

---
+++ conexia_front/src/constants/colors.js
@@ -0,0 +1,178 @@
+/*
+Principal normal: #48a6a7
+Principal hover: #419596
+Principal :active: #3a8586
+Principal light: #edf6f6
+Principal light hover: #e4f2f2
+Principal light active: #c6e3e4
+Principal dark: #367d7d
+Principal dark hover: #2b6a6a
+Principal dark active: #204b4b
+principal darker: #193a3a
+
+Secundario: #ff4953
+Secundario hover: #e6424b
+Secundario active: #cc3a42
+Secundario light: #ffedee
+Secundario light hover: #ffe4e5
+Secundario light active: #ffc7ca
+Secundario dark: #bf373e
+Secundario dark hover: #992c32
+Secundario dark active: #732125
+Secundario darker: #591a1d
+
+Escala de grices:
+Light: #f5f6f6
+Light :hover: #f1f2f2
+Light :active: #e1e4e4
+Normal: #9fa7a7
+Normal :hover: #8f9696
+Normal :active: #7f8686
+Dark: #777d7d
+Dark :hover: #5f6464
+Dark :active: #484b4b
+Darker: #383a3a
+
+Red:
+Light: #fbebea
+Light :hover: #fae0df
+Light :active: #f4bfbc
+Normal: #db3228
+Normal :hover: #c52d24
+Normal :active: #af2820
+Dark:  #a4261e
+Dark :hover:  #831e18
+Dark :active: #631612
+Darker: #4d120e
+
+Yellow:
+Light:  #fefbe8
+Light :hover: #fdf9dd
+Light :active: #fcf3b9
+Normal:  #f4d81d
+Normal :hover: #dcc21a
+Normal :active: #c3ad17
+Dark: #b7a216
+Dark :hover: #928211
+Dark :active: #6e610d
+Darker: #554c0a
+
+Green:
+Light: #ebf8ef
+Light :hover:  #e1f5e6
+Light :active: #c0eacc
+Normal: #35ba5b
+Normal :hover:  #30a752
+Normal :active: #2a9549
+Dark: #288c44
+Dark :hover: #207037
+Dark :active: #185429
+Darker: #134120
+*/
+
+export const colors = {
+  primary: {
+    normal: '#48a6a7',
+    hover: '#419596',
+    active: '#3a8586',
+    light: {
+      normal: '#edf6f6',
+      hover: '#e4f2f2',
+      active: '#c6e3e4'
+    },
+    dark: {
+      normal: '#367d7d',
+      hover: '#2b6a6a',
+      active: '#204b4b'
+    },
+    darker: '#193a3a'
+  },
+  secondary: {
+    normal: '#ff4953',
+    hover: '#e6424b',
+    active: '#cc3a42',
+    light: {
+      normal: '#ffedee',
+      hover: '#ffe4e5',
+      active: '#ffc7ca'
+    },
+    dark: {
+      normal: '#bf373e',
+      hover: '#992c32',
+      active: '#732125'
+    },
+    darker: '#591a1d'
+  },
+  gray: {
+    light: {
+      normal: '#f5f6f6',
+      hover: '#f1f2f2',
+      active: '#e1e4e4'
+    },
+    normal: {
+      normal: '#9fa7a7',
+      hover: '#8f9696',
+      active: '#7f8686'
+    },
+    dark: {
+      normal: '#777d7d',
+      hover: '#5f6464',
+      active: '#484b4b'
+    },
+    darker: '#383a3a'
+  },
+  red: {
+    light: {
+      normal: '#fbebea',
+      hover: '#fae0df',
+      active: '#f4bfbc'
+    },
+    normal: {
+      normal: '#db3228',
+      hover: '#c52d24',
+      active: '#af2820'
+    },
+    dark: {
+      normal: '#a4261e',
+      hover: '#831e18',
+      active: '#631612'
+    },
+    darker: '#4d120e'
+  },
+  yellow: {
+    light: {
+      normal: '#fefbe8',
+      hover: '#fdf9dd',
+      active: '#fcf3b9'
+    },
+    normal: {
+      normal: '#f4d81d',
+      hover: '#dcc21a',
+      active: '#c3ad17'
+    },
+    dark: {
+      normal: '#b7a216',
+      hover: '#928211',
+      active: '#6e610d'
+    },
+    darker: '#554c0a'
+  },
+  green: {
+    light: {
+      normal: '#ebf8ef',
+      hover: '#e1f5e6',
+      active: '#c0eacc'
+    },
+    normal: {
+      normal: '#35ba5b',
+      hover: '#30a752',
+      active: '#2a9549'
+    },
+    dark: {
+      normal: '#288c44',
+      hover: '#207037',
+      active: '#185429'
+    },
+    darker: '#134120'
+  }
+}; 
