4a6bdee68f9c4a26863c8a96de75c9e1b4c6852a
@paredes93871 <93871@sistemas.frc.utn.edu.ar>
2025-07-21 14:25:30 -0300

    refactor: Eliminar Admin/page.jsx

--- conexia_front/src/app/admin/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function ClientCommunity() {
-  useSessionTimeout();
-  return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold text-conexia-green">Bienvenido Administrador</h1>
-    </main>
-  );
-}
