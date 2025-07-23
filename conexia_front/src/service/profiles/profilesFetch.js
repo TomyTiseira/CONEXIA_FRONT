37cae01b55a17b67c7dfe7dba443b85079c56b45
YagoRizzetti <yagorizzetti15@gmail.com>
2025-07-23 14:51:24 -0300

    refactor: Move getProfileById function to profilesFetch.js and remove profilesFetch.jsx

--- conexia_front/src/service/profiles/profilesFetch.js
+++ conexia_front/src/service/profiles/profilesFetch.js
@@ -30,3 +30,19 @@ export async function getDocumentTypes() {
 
   return res.json();
 }
+
+export async function getProfileById(id) {
+  const res = await fetch(`${config.API_URL}/users/profile/${id}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+
+  if (!res.ok) {
+    throw new Error("No se pudo obtener el perfil");
+  }
+
+  return res.json();
+}
