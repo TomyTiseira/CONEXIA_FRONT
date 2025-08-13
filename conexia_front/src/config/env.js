280d14689d06004e65fe9d081d065ed04af51fdb
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-08-13 13:12:56 -0300

    feat: Agregar URL de documentos en la configuración del entorno para desarrollo y producción

--- conexia_front/src/config/env.js
+++ conexia_front/src/config/env.js
@@ -2,11 +2,13 @@
 const ENV = {
   development: {
     API_URL: 'http://localhost:8080/api',
-    IMAGE_URL: 'http://localhost:8080/uploads'
+    IMAGE_URL: 'http://localhost:8080/uploads',
+    DOCUMENT_URL: 'http://localhost:8080'
   },
   production: {
     API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api',
-    IMAGE_URL: process.env.NEXT_PUBLIC_IMAGE_URL || 'http://localhost:8080/uploads'
+    IMAGE_URL: process.env.NEXT_PUBLIC_IMAGE_URL || 'http://localhost:8080/uploads',
+    DOCUMENT_URL: process.env.NEXT_PUBLIC_DOCUMENT_URL || 'http://localhost:8080'
   },
 };
 
